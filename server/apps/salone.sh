cc9aa4d20647c3280355bf568a4f468f7ecc22ba
TeisD <teis.degreve@gmail.com>
2018-04-02 17:17:18 +0200

    update paths

--- server/apps/salone.sh
+++ server/apps/salone.sh
@@ -6,6 +6,10 @@
 # @reboot tmux new-session -d -s neuraltalk bash /home/ubuntu/mdw-2018/server/apps/salone.sh
 #
 
+# auto-update
+cd /home/ubuntu/mdw-2018
+git pull
+
 # start from the right folder
 cd /home/ubuntu/neuraltalk2
 
@@ -23,9 +27,9 @@ do
 	/home/ubuntu/torch/install/bin/th convert_checkpoint_gpu_to_cpu.lua ../neuraltalk2-data/checkpoints/model_id2fine.t7
 
 	# copy to the other server
-	/usr/bin/scp -i "~/mdw-2018/shared/config/keys/ec2-20180302.pem" ../neuraltalk2-data/checkpoints/model_id2fine.t7 ubuntu@ec2-18-197-19-124.eu-central-1.compute.amazonaws.com:~/neuraltalk2-data/salone.t7-new
+	/usr/bin/scp -i "/home/ubuntu/mdw-2018/shared/config/keys/ec2-20180302.pem" ../neuraltalk2-data/checkpoints/model_id2fine.t7_cpu.t7 ubuntu@ec2-18-197-19-124.eu-central-1.compute.amazonaws.com:~/neuraltalk2-data/salone.t7-new
 
 	# replace the old file on server with new one
-	/usr/bin/ssh -i "~/mdw-2018/shared/config/keys/ec2-20180302.pem" ubuntu@ec2-18-197-19-124.eu-central-1.compute.amazonaws.com -t "cd ~/neuraltalk2-data ; mv salone.t7 salone.t7-old ; mv salone.t7-new salone.t7 ; rm salone.t7-old"
+	/usr/bin/ssh -i "/home/ubuntu/mdw-2018/shared/config/keys/ec2-20180302.pem" ubuntu@ec2-18-197-19-124.eu-central-1.compute.amazonaws.com -t "cd ~/neuraltalk2-data ; mv salone.t7 salone.t7-old ; mv salone.t7-new salone.t7 ; rm salone.t7-old"
 
 done
