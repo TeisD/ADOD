724828665c272047e13db9bb85611f5ff4c5425a
TeisD <teis.degreve@gmail.com>
2018-04-02 13:27:12 +0200

    Update filenames

--- server/apps/salone.sh
+++ server/apps/salone.sh
@@ -17,13 +17,13 @@ while true
 do
 
 	# train 2500 iterations with finetuning turned on
-	/home/ubuntu/torch/install/bin/th train.lua -input_h5 data/salone-2/cocotalk.h5 -input_json data/salone-2/cocotalk.json -checkpoint_path checkpoints -id 2fine -finetune_cnn_after 0 -start_from checkpoints/model_id2.t7 -max_iters 2501 -save_checkpoint_every 2500 -batch_size 10
+	/home/ubuntu/torch/install/bin/th train.lua -input_h5 ../neuraltalk2-data/dataset/salone-2/cocotalk.h5 -input_json ../neuraltalk2-data/dataset/salone-2/cocotalk.json -checkpoint_path ../neuraltalk2-data/checkpoints -id 2fine -finetune_cnn_after 0 -start_from ../neuraltalk2-data/checkpoints/model_id2fine.t7 -max_iters 2501 -save_checkpoint_every 2500 -batch_size 10
 
 	# convert checkpoint to CPU
-	/home/ubuntu/torch/install/bin/th convert_checkpoint_gpu_to_cpu.lua checkpoints/model_id2fine.t7
+	/home/ubuntu/torch/install/bin/th convert_checkpoint_gpu_to_cpu.lua ../neuraltalk2-data/checkpoints/model_id2fine.t7
 
 	# copy to the other server
-	/usr/bin/scp -i "~/mdw-2018/shared/config/keys/ec2-20180302.pem" checkpoints/model_id2fine.t7 ubuntu@ec2-18-197-19-124.eu-central-1.compute.amazonaws.com:~/neuraltalk2-data/salone.t7-new
+	/usr/bin/scp -i "~/mdw-2018/shared/config/keys/ec2-20180302.pem" ../neuraltalk2-data/checkpoints/model_id2fine.t7 ubuntu@ec2-18-197-19-124.eu-central-1.compute.amazonaws.com:~/neuraltalk2-data/salone.t7-new
 
 	# replace the old file on server with new one
 	/usr/bin/ssh -i "~/mdw-2018/shared/config/keys/ec2-20180302.pem" ubuntu@ec2-18-197-19-124.eu-central-1.compute.amazonaws.com -t "cd ~/neuraltalk2-data ; mv salone.t7 salone.t7-old ; mv salone.t7-new salone.t7 ; rm salone.t7-old"
