133e9b15b4792275357e9a0705e14b41270e53a8
TeisD <teis.degreve@gmail.com>
2018-04-03 10:22:20 +0200

    move all processing to GPU server

--- server/apps/salone.sh
+++ server/apps/salone.sh
@@ -21,15 +21,27 @@ while true
 do
 
 	# train 2500 iterations with finetuning turned on
-	/home/ubuntu/torch/install/bin/th train.lua -input_h5 ../neuraltalk2-data/dataset/salone-2/cocotalk.h5 -input_json ../neuraltalk2-data/dataset/salone-2/cocotalk.json -checkpoint_path ../neuraltalk2-data/checkpoints -id 2fine -finetune_cnn_after 0 -start_from ../neuraltalk2-data/checkpoints/model_id2fine.t7 -max_iters 2501 -save_checkpoint_every 2500 -batch_size 10
+	/home/ubuntu/torch/install/bin/th train.lua -input_h5 ../neuraltalk2-data/dataset/salone-3/cocotalk.h5 -input_json ../neuraltalk2-data/dataset/salone-3/cocotalk.json -checkpoint_path ../neuraltalk2-data/checkpoints -id 3 -finetune_cnn_after 0 -start_from ../neuraltalk2-data/checkpoints/model_id3.t7 -max_iters 2501 -save_checkpoint_every 2500 -batch_size 10
+
+	# process all projects
+	for D in /home/ubuntu/mdw-2018-data/projects/*/; do
+		echo "Processing ${D}"
+
+		# run the network
+		/home/ubuntu/torch/install/bin/th eval.lua -model ../neuraltalk2-data/checkpoints/model_id3.t7 -image_folder "${D}" -num_images -1 -dump_images 0 -sample_max 0
+
+		# copy the results to the server
+		/usr/bin/scp -i "/home/ubuntu/mdw-2018/shared/config/keys/ec2-20180302.pem" vis/vis.json "ubuntu@ec2-18-197-19-124.eu-central-1.compute.amazonaws.com:${D}"
+
+	done
 
 	# convert checkpoint to CPU
-	/home/ubuntu/torch/install/bin/th convert_checkpoint_gpu_to_cpu.lua ../neuraltalk2-data/checkpoints/model_id2fine.t7
+	#/home/ubuntu/torch/install/bin/th convert_checkpoint_gpu_to_cpu.lua ../neuraltalk2-data/checkpoints/model_id2fine.t7
 
 	# copy to the other server
-	/usr/bin/scp -i "/home/ubuntu/mdw-2018/shared/config/keys/ec2-20180302.pem" ../neuraltalk2-data/checkpoints/model_id2fine.t7_cpu.t7 ubuntu@ec2-18-197-19-124.eu-central-1.compute.amazonaws.com:~/neuraltalk2-data/salone.t7-new
+	#/usr/bin/scp -i "/home/ubuntu/mdw-2018/shared/config/keys/ec2-20180302.pem" ../neuraltalk2-data/checkpoints/model_id2fine.t7_cpu.t7 ubuntu@ec2-18-197-19-124.eu-central-1.compute.amazonaws.com:~/neuraltalk2-data/salone.t7-new
 
 	# replace the old file on server with new one
-	/usr/bin/ssh -i "/home/ubuntu/mdw-2018/shared/config/keys/ec2-20180302.pem" ubuntu@ec2-18-197-19-124.eu-central-1.compute.amazonaws.com -t "cd ~/neuraltalk2-data ; mv salone.t7 salone.t7-old ; mv salone.t7-new salone.t7 ; rm salone.t7-old"
+	#/usr/bin/ssh -i "/home/ubuntu/mdw-2018/shared/config/keys/ec2-20180302.pem" ubuntu@ec2-18-197-19-124.eu-central-1.compute.amazonaws.com -t "cd ~/neuraltalk2-data ; mv salone.t7 salone.t7-old ; mv salone.t7-new salone.t7 ; rm salone.t7-old"
 
 done
