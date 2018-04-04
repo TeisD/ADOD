51758e14e6710bd02e198ba512af7382f8e27ab3
TeisD <teis.degreve@gmail.com>
2018-04-04 16:35:02 +0200

    update cronjob

--- server/apps/instagram.sh
+++ server/apps/instagram.sh
@@ -4,21 +4,17 @@
 # Run this file as a chron job every 5 minutes
 #
 
-# make paths relative
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-cd "$parent_path" + "/../../data/instagram"
-
 # exit if the lock file is present
-if [ ! -f lock ]; then
+if [ ! -f /home/ubuntu/mdw-2018-data/instagram/lock ]; then
     echo "Process already running!"
 		exit 0
 fi
 
 # create a lock file
-touch lock
+touch /home/ubuntu/mdw-2018-data/instagram/lock
 
 # fetch images
-instaloader --fast-update --no-videos --metadata-json "#salonedelmobile" "#milandesignweek" "#milanodesignweek" "#milandesign" "#mdw2018" "#mdw18" "#fuorisalone" "#fuorisalone2018" "#fuorisalone18" "#designweek" "#salone2018" "#salone18"
+tmux new -d -s instaloader instaloader --fast-update --no-videos --metadata-json --dirname-pattern=/home/ubuntu/mdw-2018-data/instagram/{target} "#salonedelmobile" "#milandesignweek" "#milanodesignweek" "#milandesign" "#mdw2018" "#mdw18" "#fuorisalone" "#fuorisalone2018" "#fuorisalone18" "#designweek" "#salone2018" "#salone18"
 
 # remove the lock file
-rm lock
+rm /home/ubuntu/mdw-2018-data/instagram/lock
