f755fd101d76ebe5af9067579693bbca42fa17d7
TeisD <teis.degreve@gmail.com>
2018-04-04 16:45:14 +0200

    lock file is obsolete for a detachted tmux session

--- server/apps/instagram.sh
+++ server/apps/instagram.sh
@@ -4,17 +4,5 @@
 # Run this file as a chron job every 5 minutes
 #
 
-# exit if the lock file is present
-if [ -f /home/ubuntu/mdw-2018-data/instagram/lock ]; then
-    echo "Process already running!"
-		exit 0
-fi
-
-# create a lock file
-touch /home/ubuntu/mdw-2018-data/instagram/lock
-
-# fetch images
+# fetch images & remove the lock file
 tmux new -d -s instaloader instaloader --fast-update --no-videos --metadata-json --dirname-pattern=/home/ubuntu/mdw-2018-data/instagram/{target} "#salonedelmobile" "#milandesignweek" "#milanodesignweek" "#milandesign" "#mdw2018" "#mdw18" "#fuorisalone" "#fuorisalone2018" "#fuorisalone18" "#designweek" "#salone2018" "#salone18"
-
-# remove the lock file
-rm /home/ubuntu/mdw-2018-data/instagram/lock
