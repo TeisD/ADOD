b8ab9533ecec65be27454c1ffc57614b5951cbbd
TeisD <teis.degreve@gmail.com>
2018-04-05 13:58:07 +0200

    add full path to instaloader

--- server/apps/instagram.sh
+++ server/apps/instagram.sh
@@ -5,4 +5,4 @@
 #
 
 # fetch images & remove the lock file
-tmux new -d -s instaloader instaloader --fast-update --no-videos --metadata-json --dirname-pattern=/home/ubuntu/mdw-2018-data/instagram/{target} "#salonedelmobile" "#milandesignweek" "#milanodesignweek" "#milandesign" "#mdw2018" "#mdw18" "#fuorisalone" "#fuorisalone2018" "#fuorisalone18" "#designweek" "#salone2018" "#salone18"
+/usr/bin/tmux new -d -s instaloader /home/ubuntu/.local/bin/instaloader --fast-update --no-videos --metadata-json --dirname-pattern=/home/ubuntu/mdw-2018-data/instagram/{target} "#salonedelmobile" "#milandesignweek" "#milanodesignweek" "#milandesign" "#mdw2018" "#mdw18" "#fuorisalone" "#fuorisalone2018" "#fuorisalone18" "#designweek" "#salone2018" "#salone18"
