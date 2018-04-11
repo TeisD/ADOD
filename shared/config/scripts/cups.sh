e2a83e85e0fb0bd3b5d4d42f7cd44484937cd84a
TeisD <teis.degreve@gmail.com>
2018-04-11 17:37:52 +0200

    Configuration update

---
+++ shared/config/scripts/cups.sh
@@ -0,0 +1,5 @@
+sudo ssh -f -L 60131:localhost:631 pi@raspberrypi-teis-1.local -N
+sudo ssh -f -L 60231:localhost:631 pi@raspberrypi-teis-2.local -N
+sudo ssh -f -L 60331:localhost:631 pi@raspberrypi-teis-3.local -N
+sudo ssh -f -L 60431:localhost:631 pi@raspberrypi-teis-4.local -N
+sudo ssh -f -L 60531:localhost:631 pi@raspberrypi-teis-5.local -N
