a8586fae1dbc10ba549155f1a53e27eb6c8bf303
TeisD <teis.degreve@gmail.com>
2018-04-15 16:20:14 +0200

    [instagram] slightly higher timeout

--- server/apps/instagram-search.sh
+++ server/apps/instagram-search.sh
@@ -9,4 +9,4 @@ do
 	KEYWORDS="$KEYWORDS -e '$KEYWORD'"
 done
 
-timeout 10 bash -c "find '$DIR' -type f -name *.txt -printf '%f %p\0' | sort -z -k1,1 -u -r | cut -z -f2 -d' ' | xargs -r0 grep -l --line-buffered -i $KEYWORDS | head -n$COUNT"
+timeout 15 bash -c "find '$DIR' -type f -name *.txt -printf '%f %p\0' | sort -z -k1,1 -u -r | cut -z -f2 -d' ' | xargs -r0 grep -l --line-buffered -i $KEYWORDS | head -n$COUNT"
