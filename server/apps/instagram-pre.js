4442b60488384cfffad0575fb6343726f9d3e0e8
TeisD <teis.degreve@gmail.com>
2018-09-19 23:27:02 +0200

    Extract hashtags from page

--- server/apps/instagram-pre.js
+++ server/apps/instagram-pre.js
@@ -1,26 +1,10 @@
-const mysql = require('mysql');
+const dotenv = require('dotenv');
+const path = require('path');
+const fs = require('fs');
+const _ = require('lodash');
+const Page = require('../../shared/modules/Page');
 
-const DB = 'mdw_2018';
-const TABLE = 'instagram';
-const AUTH = require('../../config/keys/mysql.json');
-
-if (!AUTH.host || !AUTH.user || !AUTH.password) throw 'Authentication file does not contain a valid password.'
-
-var db = mysql.createConnection({
-  host: AUTH.host,
-  user: AUTH.user,
-  password: AUTH.password,
-  charset : 'utf8mb4',
-  //debug: true
-});
-
-process.stdin.resume(); //so the program will not close instantly
-console.log('Connection to database...');
-db.connect(function(err) {
-  if (err) throw err
-  console.log('[OK] connected as id ' + db.threadId);
-  start();
-});
+dotenv.config();
 
 /**
  * Main function
@@ -28,36 +12,7 @@ db.connect(function(err) {
 function start() {
   const args = process.argv.slice(2);
   if (!args.length) run();
-  if (args.includes('setup')) setup();
-}
-
-/**
- * Setup the database
- */
-function setup() {
-  console.log('[INFO] Running setup script');
-  db.query('CREATE DATABASE IF NOT EXISTS '+DB, function(err){
-    if (err) throw error;
-  });
-  db.changeUser({database : DB}, function(err) {
-    if (err) throw err;
-  });
-  db.query('CREATE TABLE IF NOT EXISTS '+TABLE+' ( \
-    id BIGINT UNSIGNED PRIMARY KEY, \
-    text TEXT NOT NULL, \
-		url TEXT NOT NULL, \
-    created_at DATETIME NOT NULL, \
-		likes INT, \
-		video BOOL, \
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP)', function(err) {
-    if (err) throw err;
-  });
-  db.query('ALTER DATABASE '+DB+' CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci', function(err) {
-    if (err) throw err;
-  });
-  db.query('ALTER TABLE '+TABLE+' CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci', function(err) {
-    if (err) throw err;
-  });
+  if (args.includes('extract')) extract();
 }
 
 /**
@@ -67,6 +22,36 @@ function run() {
 	console.log('[INFO] Main logic is not implemented');
 	console.log('run instaloader instead');
 	console.log('instaloader --fast-update --no-videos --metadata-json "#salonedelmobile" "#milandesignweek" "#milanodesignweek" "#milandesign" "#mdw2018" "#mdw18" "#fuorisalone" "#fuorisalone2018" "#fuorisalone18" "#designweek" "#salone2018" "#salone18"');
+}
+
+/**
+ * Extract keywords from pages
+ */
+function extract() {
+  console.log('Starting extraction');
+  var pages = Page.loadFolder(path.join(process.env.DATA_DIR, 'pages'));
+
+  let keywords = [];
+
+  pages.forEach(page => {
+    page.blocks.forEach(block => {
+      block.lines.forEach(line => {
+        if(!line.hasOwnProperty('hashtags')) return;
+        line.hashtags.forEach(hashtag => {
+          keywords.push(hashtag);
+        })
+      })
+    })
+  });
+
+  keywords = _.uniq(keywords);
+
+  keywords = '"#' + keywords.join('" "#') + '"';
+
+  let out = path.join(process.env.DATA_DIR, 'instagram', 'hashtags.json');
+
+  fs.writeFileSync(out, keywords);
+  console.log('Saved hashtags to ' + out);
 }
 
 // search
@@ -80,3 +65,5 @@ function run() {
 // -w -> word match
 // -i -> ignore case
 // -Z -> zero byte (for piping)
+
+start();
