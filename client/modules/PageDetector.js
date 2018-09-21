b202fe2af0de2f4f70673edccc58d98bfb22849b
TeisD <teis.degreve@gmail.com>
2018-09-21 12:41:08 +0200

    typo

--- client/modules/PageDetector.js
+++ client/modules/PageDetector.js
@@ -160,19 +160,19 @@ class PageDetector extends EventEmitter {
 		im = im.adaptiveThreshold(255, 1, 0, 1001, 20);
 		var _im = im.copy();
 		if(process.env.DEBUGGING) im.save('mid-thresh.jpg');
-		console.log('<PD> Countour START');
+		console.log('<PD> Contour START');
 
 		var contours = im.findContours();
 		var id = 0;
 		var difference = +Infinity;
-		console.log('<PD> Countour END');
+		console.log('<PD> Contour END');
 
 
 		if(!contours.size()) {
 			return Promise.reject(STATUS.NO_PAGE);
 		}
 
-		console.log('<PD> Countour size: ' + countours.size());
+		console.log('<PD> Contour size: ' + contours.size());
 		for (let i = 0; i < contours.size(); i++) {
 			var d = Math.abs(contours.area(i) - AREA);
 			if(d < difference) {
