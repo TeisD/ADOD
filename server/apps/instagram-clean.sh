511dd9bd1f20061dcd53b59937c5e5b9ef66220e
Teis <teis@teis.me>
2019-05-01 10:44:21 +0200

    Clean script for instagram

---
+++ server/apps/instagram-clean.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+DIR=$1
+SIZE=500
+
+cd "$DIR"
+
+for d in */; do
+    array=()
+    
+    find "$d" -type f -name *.txt -print0 | sort -z >tmpfile
+    while IFS=  read -r -d $'\0'; do
+        filename=$(basename -- "$REPLY")
+        filename="${filename%.*}"
+        array+=("$filename")
+    done <tmpfile
+    rm -f tmpfile
+
+    # calculate number of files to remove
+    l=$(( ${#array[@]} - $SIZE ))
+    l=$(( $l > 0 ? $l : 0 ))
+    
+    echo "Removing $l files from $d"
+
+    for (( i=0; i<$l; i++ ))
+    do
+        rm $DIR/$d/${array[$i]}*
+    done
+    
+done
+
+echo "Done";
