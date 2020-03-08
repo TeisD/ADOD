f047bb083154673851c3d01498bb912ecc2dbf5e
Teis <teis@teis.me>
2020-03-08 19:44:08 +0100

    Init canvas

--- client/controllers/Z33.js
+++ client/controllers/Z33.js
@@ -13,12 +13,15 @@ class Test extends Controller {
     load(page, rotate) {
         this.instagram.load(page, rotate);
         this.twitter.load(page, rotate);
+
+        this.canvas = this.twitter.canvas;
+        this.ctx = this.twitter.ctx;
     }
 
 	draw(data) {
         this.twitter.draw(data[1]);
-        this.instagram.canvas = this.twitter.canvas;
-        this.instagram.ctx = this.twitter.ctx;
+        this.instagram.canvas = this.canvas;
+        this.instagram.ctx = this.ctx;
 
         return this.instagram.draw(data[0]).then(() => {
             this.canvas = this.instagram.canvas;
